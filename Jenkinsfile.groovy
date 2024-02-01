fee365f704cf638d07e7b30aeeabe7e5ac6a57e6
Mohammed Jaleel <mohjalee@publicisgroupe.net>
2024-02-01 16:13:47 +0530

    added groovy file with build

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,22 @@
 pipeline {
     agent any
     stages {
+        stage('Build') {
+            steps {
+                // Build your code, for example using Maven or Gradle
+                sh 'mvn clean install'
+                }
+            }
         stage('Deploy to AEM') {
             steps {
                 // Deploy your code to AEM, for example using CURL commands
                 // sh 'curl -u admin:admin -X POST -F package=@"path/to/your/package.zip" http://13.49.72.14:4502/crx/packmgr/service.jsp'
-                sh 'curl -u admin:admin -F cmd=install http://13.49.72.14:4502/crx/packmgr/service/.json/etc/packages/com.employdemy/employdemy.ui.content-1.0.0-SNAPSHOT.zip '
-                sh 'curl -u admin:admin -F cmd=install http://13.49.72.14:4502/crx/packmgr/service/.json/etc/packages/com.employdemy/employdemy.ui.config-1.0.0-SNAPSHOT.zip'
-                sh 'curl -u admin:admin -F cmd=install http://13.49.72.14:4502/crx/packmgr/service/.json/etc/packages/com.employdemy/employdemy.ui.apps-1.0.0-SNAPSHOT.zip'
+                sh 'curl -u admin:admin -X POST -F package=@".json/etc/packages/com.employdemy/employdemy.ui.apps-1.0.0-SNAPSHOT.zip" http://13.49.72.14:4502/crx/packmgr/service.jsp'
+                sh 'curl -u admin:admin -X POST -F package=@".json/etc/packages/com.employdemy/employdemy.ui.config-1.0.0-SNAPSHOT.zip" http://13.49.72.14:4502/crx/packmgr/service.jsp'
+                sh 'curl -u admin:admin -X POST -F package=@".json/etc/packages/com.employdemy/employdemy.ui.content-1.0.0-SNAPSHOT.zip" http://13.49.72.14:4502/crx/packmgr/service.jsp'
+                // sh 'curl -u admin:admin -F cmd=install http://13.49.72.14:4502/crx/packmgr/service/.json/etc/packages/com.employdemy/employdemy.ui.content-1.0.0-SNAPSHOT.zip '
+                // sh 'curl -u admin:admin -F cmd=install http://13.49.72.14:4502/crx/packmgr/service/.json/etc/packages/com.employdemy/employdemy.ui.config-1.0.0-SNAPSHOT.zip'
+                // sh 'curl -u admin:admin -F cmd=install http://13.49.72.14:4502/crx/packmgr/service/.json/etc/packages/com.employdemy/employdemy.ui.apps-1.0.0-SNAPSHOT.zip'
                 }
         }
     }
